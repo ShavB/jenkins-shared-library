bc0b1a17691cfeb6f8b54ff2e819debc220f66f1
ShyamBisht <bishtshyam135@gmail.com>
2025-05-13 17:25:24 +0100

    fixed file path

---
+++ vars/docker_creds.groovy
@@ -0,0 +1,13 @@
+def call(String project_name, String image_tag, String docker_user) {
+    withCredentials([usernamePassword(
+        'credentialsId': 'docker-hub-cred',
+        passwordVariable: 'dockerHubPass',
+        usernameVariable: 'dockerhubUser')
+    ]) {
+        sh """
+        docker login -u ${docker_user} -p ${dockerHubPass}
+        docker image tag ${project_name}:${image_tag} ${docker_user}/${project_name}:${image_tag}
+        docker push ${docker_user}/${project_name}:${image_tag}
+        """
+    }
+}
