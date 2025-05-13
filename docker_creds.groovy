488c36020ac54d94d4567475243b5b557cbea078
ShyamBisht <bishtshyam135@gmail.com>
2025-05-13 17:21:14 +0100

    updated docker creds

--- docker_creds.groovy
+++ docker_creds.groovy
@@ -3,10 +3,11 @@ def call(String project_name, String image_tag, String docker_user) {
         'credentialsId': 'docker-hub-cred',
         passwordVariable: 'dockerHubPass',
         usernameVariable: 'dockerhubUser')
-        ])
-        {
-        sh "docker login -u ${docker_user} -p ${dockerHubPass}"
-        sh "docker image tag ${project_name}:${image_tag} ${docker_user}/${project_name}:${image_tag}"
-        sh "docker push ${docker_user}/${project_name}:${image_tag}"
-        }
+    ]) {
+        sh """
+        docker login -u ${docker_user} -p ${dockerHubPass}
+        docker image tag ${project_name}:${image_tag} ${docker_user}/${project_name}:${image_tag}
+        docker push ${docker_user}/${project_name}:${image_tag}
+        """
+    }
 }
